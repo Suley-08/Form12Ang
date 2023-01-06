a26216d7a01a747c5252a998d9a2f380a0b4c598
Karen Rivera <karenriverah.18@gmail.com>
2023-01-06 01:44:25 -0500

    formReactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
+    ContactReactiveComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,  
+    FormsModule, 
+    ReactiveFormsModule 
   ],
   exports: [
     ContactComponent
